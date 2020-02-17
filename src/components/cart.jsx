b4529289118022092b3da82e6b8df828f0262744
System Administrator <root@Arshads-Mac-mini.local>
2020-02-17 11:56:07 +0530

    fix issue

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Cart(props) {
+  let history = useHistory();
+  function handleClick() {
+    history.push("/cart");
+  }
   return (
     <React.Fragment>
-      <a href="/cart">
+      <a onClick={handleClick} href="#">
         <div id="ex4">
           <i className="fa fa-shopping-cart text-white" aria-hidden="true"></i>
           <span className="badge badge-warning" id="lblCartCount">
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Search from "./search";
 import Cart from "./cart";
+import { useHistory } from "react-router-dom";
 
 function Navbar(props) {
+  let history = useHistory();
+  function handleClick() {
+    history.push("/");
+  }
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-      <a className="navbar-brand" href="/">
+      <a onClick={handleClick} className="navbar-brand" href="#">
         <i className="fa fa-star text-warning" aria-hidden="true"></i>
       </a>
       <Search handleSearch={props.handleSearch} />
